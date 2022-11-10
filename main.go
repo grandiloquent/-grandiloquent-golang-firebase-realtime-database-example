18c6854990ef38c0dc4aa0aea3c544299babd45c
grandiloquent <1785907865@qq.com>
2022-11-10 11:43:27 +0800

    更新

--- main.go
+++ main.go
@@ -16,20 +16,4 @@ func main() {
 			share.ListArticles(ref, w, r)
 		}
 	}))
-	// var data map[string]interface{}
-	// if err := ref.Child("").Get(ctx, &data); err != nil {
-	// 	log.Fatalln("Error reading from database:", err)
-	// }
-	// m := make(map[string]interface{})
-	// m["updateAt"] = time.Now().Unix()
-	// d, err := ref.Child("articles").Push(ctx, m)
-	// fmt.Println(d.Key)
-	// q := ref.Child("articles").OrderByChild("updateAt")
-	// qn, err := q.GetOrdered(ctx)
-	// for _, qx := range qn {
-	// 	var x map[string]interface{}
-	// 	ref.Child("articles/"+qx.Key()).Get(ctx, &x)
-	// 	fmt.Println(x)
-	// }
-
 }
